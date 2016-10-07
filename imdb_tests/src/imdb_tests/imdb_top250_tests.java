5ab2cd5b0164ae86b475f5d5cfe56fbee25f1ebb
mv-test-account <venosmichal@gmail.com>
2016-10-07 10:54:31 +0200

    adding chromedrive and external jars

--- imdb_tests/src/imdb_tests/imdb_top250_tests.java
+++ imdb_tests/src/imdb_tests/imdb_top250_tests.java
@@ -4,6 +4,8 @@ import static org.testng.AssertJUnit.assertEquals;
 import static org.testng.AssertJUnit.assertTrue;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -22,72 +24,77 @@ import org.testng.annotations.Test;
 
 public class imdb_top250_tests {
 	public WebDriver driver;
-
+	public List<WebElement> listOfMovies;
 	
 	  @Test(description="Check if at least 1 movie is in the list of movies on the IMDb page top 250 ")
-	  public void testOfTop250() {
-		  List<WebElement> listOfMovies = driver.findElements(By.className("titleColumn"));
-		
+	  public void testOfTop250() throws Exception {
+		  
+		  try{
+			  listOfMovies = driver.findElements(By.className("titleColumn"));
+			  } catch(Exception e){
+				  System.out.println(e);
+				  getscreenshot("testOfTop250");
+			  }
+		  		
 		  System.out.println("Number of items in movie list is:" + listOfMovies.size());
-		  assertTrue(listOfMovies.size() >1);
+		  assertTrue(listOfMovies.size() >=1);
 	  }
 	  
-	  @Test(description="Try to sort by any available option in the Sort by dropdown option on page Top rated movies and check if at least 1 movie is in the list")
-	  public void testOfSorting() {
-		Select dropdown = new Select(driver.findElement(By.name("sort")));
-		List<WebElement> sortOption = dropdown.getOptions();
-		
-		for(WebElement e : sortOption){
+	  @Test(description="Try to sort by any available option in the Sort by dropdown on page Top rated movies and check if at least 1 movie is in the list")
+	  public void testOfSorting() throws Exception {
+		  Select dropdown = null; 
+		  List<WebElement> sortOption = null;
+		  
+		try{
+			dropdown = new Select(driver.findElement(By.name("sort")));
+			sortOption = dropdown.getOptions();
 			
+			  } catch(Exception e){
+				  System.out.println(e);
+				  getscreenshot("testOfSorting");
+			  }
+			  
+		for(WebElement e : sortOption){			
 			dropdown.selectByVisibleText(e.getText());
-			List<WebElement> listOfMovies = driver.findElements(By.className("titleColumn"));
+			listOfMovies = driver.findElements(By.className("titleColumn"));
 			System.out.println("test of sortby: " + e.getText());
 			assertTrue(listOfMovies.size() >1);
-		}
-		
+		}		
 	  }
 	  
 	  @Test(description="Try to click on genre western and display and check if at least 1 movie is in the list")
-
 	  public void testOfGenre()throws Exception  {
 		  try{
 		  driver.findElement(By.linkText("Western")).click();
 		  } catch(Exception e){
-			  
-			  getscreenshot();
+			  System.out.println(e);
+			  getscreenshot("testOfGenre");
 		  }
 		  
-		  List<WebElement> listOfMovies = driver.findElements(By.className("lister-item-header"));
-			
+		  listOfMovies = driver.findElements(By.className("lister-item-header"));			
 		  System.out.println("Number of items in movie list is:" + listOfMovies.size());
 		  assertTrue(listOfMovies.size() >1);
 	  }
 	  
-	  public void getscreenshot() throws Exception 
-	  {
-	          File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-	       //The below method will save the screen shot in d drive with name "screenshot.png"
-	          FileUtils.copyFile(scrFile, new File("D:\\screenshot.png"));
+	  public void getscreenshot(String name) throws Exception 
+	  {		
+	         File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);	  
+	         String destDir = System.getProperty("user.dir") + "/" + "test-output/screenshots";
+	         new File(destDir ).mkdirs();
+	         String destFile = destDir + "/" + name +  new SimpleDateFormat("MM-dd-yyyy_HH-ss").format(new GregorianCalendar().getTime()) + ".png";
+	         FileUtils.copyFile(scrFile, new File(destFile));
 	  }
 	  
 	  
 	  @BeforeSuite
 	  public void beforeMethod() {
-			
-			//File file = new File("C:/selenium/java/geckodriver.exe");
-			//System.setProperty("webdriver.gecko.driver",file.getAbsolutePath());
-			//driver = new FirefoxDriver();
-			
 			System.setProperty("webdriver.chrome.driver", "C:/selenium/chromedriver.exe");
 			driver = new ChromeDriver();
 			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 			driver.get("http://www.imdb.com/chart/top");
-			//assertEquals( driver.getTitle(), "IMDb Top 250 - IMDb", "IMDb page was not loaded properly");
+			assertEquals( driver.getTitle(), "IMDb Top 250 - IMDb");
 	  }
 	  
-	  
-	  
-
 	  @AfterSuite
 	  public void afterMethod() {	  
 		  driver.quit();
