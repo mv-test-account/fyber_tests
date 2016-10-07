89c9bf300d6091ba8cd8c4ad0a648ab5a33a9937
mv-test-account <venosmichal@gmail.com>
2016-10-07 08:35:11 +0200

    update

--- imdb_tests/src/imdb_tests/imdb_top250_tests.java
+++ imdb_tests/src/imdb_tests/imdb_top250_tests.java
@@ -16,9 +16,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
